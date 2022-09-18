481aa31d896245458ccc5988fd4baadd17ab916e
Jaime Terreu <jaime@terreu.com>
2022-09-18 16:52:26 +0930

    Add basic tests

--- tests/e2e2.spec.ts
+++ tests/e2e2.spec.ts
@@ -22,4 +22,49 @@ describe("examples", () => {
 
     expect(transitionExampleVisible).toBeTruthy();
   });
+
+  describe("Transition example", () => {
+    it("Does not show the content by default", async () => {
+      await browser.newContext();
+      const page = await browser.newPage();
+      await page.goto(`${BASE_URI}/Transition.elm`);
+
+      const contentVisible = await page.isVisible(
+        "[data-test-id=animate-height-content]"
+      );
+
+      expect(contentVisible).toBeFalsy();
+    });
+
+    it("displays content when animation ends", async () => {
+      await browser.newContext();
+      const page = await browser.newPage();
+      await page.goto(`${BASE_URI}/Transition.elm`);
+
+      await page.click("button:has-text('Toggle')");
+      await page.waitForSelector("[data-test-id=animate-height-content]");
+      const contentVisible = await page.isVisible(
+        "[data-test-id=animate-height-content]"
+      );
+
+      expect(contentVisible).toBeTruthy();
+    });
+
+    it("Hides the content when togling after content shows", async () => {
+      await browser.newContext();
+      const page = await browser.newPage();
+      await page.goto(`${BASE_URI}/Transition.elm`);
+
+      await page.click("button:has-text('Toggle')");
+      await page.waitForSelector("[data-test-id=animate-height-content]");
+      await page.waitForTimeout(400);
+      await page.click("button:has-text('Toggle')");
+      await page.waitForTimeout(400);
+      const contentVisible = await page.isVisible(
+        "[data-test-id=animate-height-content]"
+      );
+
+      expect(contentVisible).toBeFalsy();
+    });
+  });
 });
