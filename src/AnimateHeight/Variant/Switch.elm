3c02152e186ee1aef6091078a8bffeda9867b1ec
Jaime Terreu <jaime@Jaimes-MBP.lan>
2023-05-24 23:09:31 +0930

    Switch views

--- src/AnimateHeight/Variant/Switch.elm
+++ src/AnimateHeight/Variant/Switch.elm
@@ -16,6 +16,8 @@ import Browser.Dom as Dom
 import Browser.Events as DomEvents
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (attribute, style)
+import Html.Events as HtmlEvents
+import Json.Decode as Decode
 import Task
 
 
@@ -23,6 +25,9 @@ type Msg
     = GhostNoOp AnimateHeight.Msg
     | AnimateHeightMsg AnimateHeight.Msg
     | QueryGhostMsg
+    | SwitchView
+    | AnimationStart
+    | AnimationEnd
     | GotGhostElement (Result Dom.Error Dom.Viewport)
 
 
@@ -50,8 +55,8 @@ type GhostState
 type IncomingView view
     = Ghost view
     | QueryingGhost view
-    | Placed ( view, Float )
     | Entering view
+    | Entered view
 
 
 type Config msg
@@ -101,8 +106,11 @@ subscriptions (State state_) =
     Sub.batch
         [ Sub.map AnimateHeightMsg (AnimateHeight.subscriptions state_.ahState)
         , case state_.incomingView of
-            Just (Ghost _) ->
+            Just (Ghost v) ->
                 DomEvents.onAnimationFrame (\_ -> QueryGhostMsg)
+
+            Just (Entered _) ->
+                DomEvents.onAnimationFrame (\_ -> SwitchView)
 
             _ ->
                 Sub.none
@@ -112,6 +120,25 @@ subscriptions (State state_) =
 update : Msg -> State view -> ( State view, Cmd Msg )
 update msg1 ((State state_) as st) =
     case msg1 of
+        SwitchView ->
+            case state_.incomingView of
+                Just (Entered v) ->
+                    ( State { state_ | incomingView = Nothing, currentView = Just v }, Cmd.none )
+
+                _ ->
+                    ( State { state_ | incomingView = Nothing }, Cmd.none )
+
+        AnimationStart ->
+            ( st, Cmd.none )
+
+        AnimationEnd ->
+            case state_.incomingView of
+                Just (Entering v) ->
+                    ( State { state_ | incomingView = Just (Entered v) }, Cmd.none )
+
+                _ ->
+                    ( st, Cmd.none )
+
         AnimateHeightMsg msg2 ->
             let
                 ( _, updatedState, cmds ) =
@@ -151,25 +178,27 @@ update msg1 ((State state_) as st) =
                     , Cmd.none
                     )
 
-                ( Just v1, Just _ ) ->
-                    ( State { state_ | incomingView = Just (QueryingGhost v1) }, queryCmd )
+                ( Just v1, Just v2 ) ->
+                    if v1 == v2 then
+                        ( State { state_ | incomingView = Nothing }, Cmd.none )
 
-                -- TODO Handle Placed incoming view
-                -- ( Just v1, Just v2 ) ->
+                    else
+                        ( State { state_ | incomingView = Just (QueryingGhost v1) }, queryCmd )
+
                 ( Nothing, _ ) ->
                     ( State { state_ | incomingView = Nothing }, Cmd.none )
 
         GotGhostElement (Ok elem) ->
-            let
-                _ =
-                    Debug.log "OK" elem
-            in
             case state_.incomingView of
                 Just (QueryingGhost v) ->
                     ( State
                         { state_
                             | incomingView =
-                                Just (Placed ( v, elem.scene.height ))
+                                Just (Entering v)
+                            , ahState =
+                                AnimateHeight.height
+                                    (AnimateHeight.fixed elem.scene.height)
+                                    state_.ahState
                         }
                     , Cmd.none
                     )
@@ -193,14 +222,64 @@ view toView (Config config) (State state_) =
                 |> AnimateHeight.content
                     (case state_.currentView of
                         Just v ->
+                            let
+                                ( incomingOpac, currentOpac ) =
+                                    case state_.incomingView of
+                                        Just (QueryingGhost _) ->
+                                            ( [ style "opacity" "0" ], [ style "opacity" "1" ] )
+
+                                        Just (Entering _) ->
+                                            ( [ style "opacity" "1" ], [ style "opacity" "0" ] )
+
+                                        Just (Entered _) ->
+                                            ( [], [ style "display" "none" ] )
+
+                                        _ ->
+                                            ( [], [] )
+                            in
                             [ div [ style "position" "relative" ]
                                 [ case state_.incomingView of
-                                    Just (Placed ( v1, _ )) ->
-                                        div [ style "opacity" "0", style "position" "absolute" ] (toView v1)
+                                    Just (QueryingGhost v1) ->
+                                        div
+                                            ([ style "position" "absolute"
+                                             , style "transition-property" "opacity"
+                                             , style "transition-timing-function" "ease-in"
+                                             , style "transition-duration" "200ms"
+                                             , HtmlEvents.on "transitionend" (Decode.succeed (config.inject AnimationEnd))
+                                             ]
+                                                ++ incomingOpac
+                                            )
+                                            (toView v1)
+
+                                    Just (Entering v1) ->
+                                        div
+                                            ([ style "position" "absolute"
+                                             , style "transition-property" "opacity"
+                                             , style "transition-timing-function" "ease-in"
+                                             , style "transition-duration" "200ms"
+                                             , HtmlEvents.on "transitionend" (Decode.succeed (config.inject AnimationEnd))
+                                             ]
+                                                ++ incomingOpac
+                                            )
+                                            (toView v1)
+
+                                    Just (Entered v1) ->
+                                        div
+                                            [ style "position" "absolute"
+                                            ]
+                                            (toView v1)
 
                                     _ ->
                                         text ""
-                                , div [ style "position" "absolute" ] (toView v)
+                                , div
+                                    ([ style "position" "absolute"
+                                     , style "transition-property" "opacity"
+                                     , style "transition-timing-function" "ease-in"
+                                     , style "transition-duration" "200ms"
+                                     ]
+                                        ++ currentOpac
+                                    )
+                                    (toView v)
                                 ]
                             ]
 
